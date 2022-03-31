8db7c8fe241426aea10de92295ce8c6897c562b6
ANormalDude <61946126+WasimIbKhan@users.noreply.github.com>
2022-03-31 16:53:57 +0100

    Revert "clean up code structure"
    This reverts commit e0b24c23e6ea7c409fab038c2ef8e7efc7b7f13f.

--- src/App.js
+++ src/App.js
@@ -16,8 +16,7 @@ import Navigation from './Components/MainNavigation'
 import Schedule from './Pages/ScheduleTab/Schedule'
 import Profile from './Pages/ProfileTab/Profile'
 import EditProfile from './Pages/ProfileTab/EditProfile'
-import OrganisationMembers from './Pages/MenuTab/OrganisationMembers'
-import Member from './Pages/MenuTab/Member'
+
 
 const fakeAuth = () =>
   new Promise((resolve) => {
@@ -72,6 +71,39 @@ const ProtectedRoute = ({ children }) => {
 
   return children;
 };
+
+
+const BlogPosts = {
+  '1': {
+    name: 'Wasim',
+    description: 'Is meant to work on the Schedule Tab, hes completed it as now you can add any task to the schedule'
+  },
+  '2': {
+    name: 'Amr Khalid A Bawzeer',
+    description: 'Is meant to work on the profile UI and Edit Profile UI'
+  },
+  '3': {
+    name: 'Aadil Sayed',
+    description: 'Is meant to work on the Login Page UI'
+  },
+  '4': {
+    name: 'Haotian Chen',
+    description: 'Is meant to work on the Signup page UI'
+  },
+  '5': {
+    name: 'Santhya Kugathas',
+    description: 'Is meant to work on the Menu Tab, which is suppost to be able to assign user permissions and assign group members to a line manager'
+  },
+  '6': {
+    name: 'Naivedhya Premal Shah',
+    description: 'Is meant to work on the Menu Tab, which is suppost to be able to assign user permissions and assign group members to a line manager'
+  },
+  '7': {
+    name: 'Venkata Stripada Gayatri Sistla',
+    description: 'Is meant to work on the Menu Tab, which is suppost to be able to assign user permissions and assign group members to a line manager'
+  },
+};
+
 
 function App() {
   return (
@@ -147,4 +179,35 @@ const Dashboard = () => {
   )
 }
 
+function OrganisationMembers() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h2>Organisation Members</h2>
+      <ul>
+      {Object.entries(BlogPosts).map(([slug, { name }]) => (
+        <li key={slug}>
+          <Link to={`${slug}`}>
+            <h3>{name}</h3>
+          </Link>
+        </li>
+      ))}
+      </ul>
+    </div>
+    
+  );
+}
+
+function Member() {
+  const { slug } = useParams();
+  const member = BlogPosts[slug];
+
+  const { name, description } = member;
+
+  return (
+    <div style={{ padding: 20 }}>
+      <h3>{name}</h3>
+      <p>{description}</p>
+    </div>
+  );
+}
 export default App;
