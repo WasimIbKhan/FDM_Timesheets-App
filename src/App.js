653b732458ed03a4ecd9e623618d28a63c3e2a43
Naivedhya13 <44753319+Naivedhya13@users.noreply.github.com>
2022-04-10 14:55:31 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -204,7 +204,7 @@ const Dashboard = () => {
 
 function OrganisationMembers() {
   return (
-    <div>
+    <div className="html">
         <meta charSet="UTF-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
