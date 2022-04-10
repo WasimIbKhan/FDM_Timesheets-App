8c7930a3acca4448a1d06b0ad23ffb715ec8dc1c
Naivedhya13 <44753319+Naivedhya13@users.noreply.github.com>
2022-04-10 16:57:21 +0100

    change

--- src/App.js
+++ src/App.js
@@ -213,7 +213,7 @@ function OrganisationMembers() {
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
         {/*Bootstrap 4*/}
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossOrigin="anonymous" />
-         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
+         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css" />
 
         <div className="container">
           <div className="row">
