2cc2f64a8ecdea023cd5080ebf50109d06272d7d
ANormalDude <61946126+WasimIbKhan@users.noreply.github.com>
2022-04-13 21:10:51 +0100

    Revert "Revert "Menulist back-end complete""
    This reverts commit 22308778fb38195c27e3b3fccf317e9a21e7990b.

--- src/Pages/ProfileTab/EditProfile.js
+++ src/Pages/ProfileTab/EditProfile.js
@@ -1,29 +1,21 @@
-import React from "react";
+import React,{useState} from "react";
 import "./EditProfile.css";
+import { useNavigate } from 'react-router-dom';
 
 function EditProfile() {
+  const [file, setFile] = useState();
+  function handleChange(e) {
+      console.log(e.target.files);
+      setFile(URL.createObjectURL(e.target.files[0]));
+  }
+
   return (
-    <div className="Card">
-      <div className="upper-container">
-        <div className="image-container">
-          <img
-            src="https://www.corporatephotographylondon.com/wp-content/uploads/2019/11/HKstrategies-1029-1024x683.jpg"
-            alt=" "
-            height="100px"
-            width="100px"
-            class="cropped-image"
-          />
-        </div>
-      </div>
-      <div className="lower-container">
-        <h4> Alex Damon </h4>
-        <h4> Team Leader </h4>
-        <h4> Employed Since: </h4>
-        <h4> 2018 </h4>
-        <h3> Site Location: On-Site </h3>
-        <button> Edit Site Location </button>
-      </div>
+    <div className="App">
+      <h2>Add Image:</h2>
+      <input type="file" onChange={handleChange} />
+      <img src={file} />
     </div>
+
   );
 }
 
